a4b9956723d4cbe1b789c991e16f52c264cdbdef
pachipaz <raquelpaz.gutierrez@gmail.com>
2016-10-04 00:33:00 -0300

    cachipum inicio

---
+++ variables.js
@@ -0,0 +1,19 @@
+/* 0=piedra/1=tijera/2=papel*/
+
+/*opciones disponibles para el juego */ 
+if (numeroRedondo===0){
+	jugadaCompu = "piedra";
+}else {
+	jugadaCompu = "tijera";
+}else {
+	jugadaCompu = "papel";
+}
+}
+
+alert("Ahora juega el computador");
+console.log (jugadaCompu);
+alert ("El computador eligio:"+jugadaCompu);
+
+
+
+
